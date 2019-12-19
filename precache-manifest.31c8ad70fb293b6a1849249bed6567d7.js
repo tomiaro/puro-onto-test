bcfc733585368a40ee280e22c0ddd1a81caae6d1
tomiaro <tmmorkus@gmail.com>
2019-12-19 20:54:05 +0100

    test

---
+++ precache-manifest.31c8ad70fb293b6a1849249bed6567d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa5fc0bc5c89dabfc486bdd7de1ab77",
+    "url": "/puro-onto-test/index.html"
+  },
+  {
+    "revision": "f178b9e6fa35bf6e7c76",
+    "url": "/puro-onto-test/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "662a38e6bdb1c87dc6e3",
+    "url": "/puro-onto-test/static/css/main.d4bc68e5.chunk.css"
+  },
+  {
+    "revision": "f178b9e6fa35bf6e7c76",
+    "url": "/puro-onto-test/static/js/2.541900cc.chunk.js"
+  },
+  {
+    "revision": "662a38e6bdb1c87dc6e3",
+    "url": "/puro-onto-test/static/js/main.77143f8b.chunk.js"
+  },
+  {
+    "revision": "14a68028665ef0745172",
+    "url": "/puro-onto-test/static/js/runtime-main.c4df2994.js"
+  }
+]);
